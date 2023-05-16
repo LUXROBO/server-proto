5ee6aa1b5d7cd4c56e9f65d805f5c3966373541d
csk6124 <csk6124@gmail.com>
2023-05-16 12:23:56 +0900

    feat: predict 메소드 추가

--- gen/python/predict/predict_pb2.py
+++ gen/python/predict/predict_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='predict',
   syntax='proto3',
   serialized_options=_b('\n\023com.luxrobo.predictB\021RecommendApiProtoP\001Z\017luxrobo/predict\242\002\003TPX\252\002\017Luxrobo.predict\312\002\017Luxrobo\\predict'),
-  serialized_pb=_b('\n\x15predict/predict.proto\x12\x07predict\"%\n\x07Numbers\x12\x0c\n\x04unum\x18\x01 \x01(\x05\x12\x0c\n\x04qnum\x18\x02 \x01(\x05\"<\n\x0bPredictions\x12\x10\n\x08passrate\x18\x01 \x01(\x02\x12\x0c\n\x04time\x18\x02 \x01(\x05\x12\r\n\x05score\x18\x03 \x01(\x02\x32\x43\n\tPredictor\x12\x36\n\nGetPredict\x12\x10.predict.Numbers\x1a\x14.predict.Predictions\"\x00\x42\x65\n\x13\x63om.luxrobo.predictB\x11RecommendApiProtoP\x01Z\x0fluxrobo/predict\xa2\x02\x03TPX\xaa\x02\x0fLuxrobo.predict\xca\x02\x0fLuxrobo\\predictb\x06proto3')
+  serialized_pb=_b('\n\x15predict/predict.proto\x12\x07predict\"%\n\x07Numbers\x12\x0c\n\x04unum\x18\x01 \x01(\x05\x12\x0c\n\x04qnum\x18\x02 \x01(\x05\"<\n\x0bPredictions\x12\x10\n\x08passrate\x18\x01 \x01(\x02\x12\x0c\n\x04time\x18\x02 \x01(\x05\x12\r\n\x05score\x18\x03 \x01(\x02\"`\n\x14RecommandQuizRequest\x12\x12\n\nprofile_id\x18\x01 \x01(\t\x12\x0e\n\x06target\x18\x02 \x01(\t\x12\x12\n\ndifficulty\x18\x03 \x01(\t\x12\x10\n\x08language\x18\x04 \x01(\t\"-\n\x15RecommandQuizResponse\x12\x14\n\x0cquestion_ids\x18\x01 \x03(\x05\x32\x95\x01\n\tPredictor\x12\x36\n\nGetPredict\x12\x10.predict.Numbers\x1a\x14.predict.Predictions\"\x00\x12P\n\rRecommandQuiz\x12\x1d.predict.RecommandQuizRequest\x1a\x1e.predict.RecommandQuizResponse\"\x00\x42\x65\n\x13\x63om.luxrobo.predictB\x11RecommendApiProtoP\x01Z\x0fluxrobo/predict\xa2\x02\x03TPX\xaa\x02\x0fLuxrobo.predict\xca\x02\x0fLuxrobo\\predictb\x06proto3')
 )
 
 
@@ -108,8 +108,93 @@ _PREDICTIONS = _descriptor.Descriptor(
   serialized_end=133,
 )
 
+
+_RECOMMANDQUIZREQUEST = _descriptor.Descriptor(
+  name='RecommandQuizRequest',
+  full_name='predict.RecommandQuizRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='profile_id', full_name='predict.RecommandQuizRequest.profile_id', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='target', full_name='predict.RecommandQuizRequest.target', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='difficulty', full_name='predict.RecommandQuizRequest.difficulty', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='language', full_name='predict.RecommandQuizRequest.language', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=135,
+  serialized_end=231,
+)
+
+
+_RECOMMANDQUIZRESPONSE = _descriptor.Descriptor(
+  name='RecommandQuizResponse',
+  full_name='predict.RecommandQuizResponse',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='question_ids', full_name='predict.RecommandQuizResponse.question_ids', index=0,
+      number=1, type=5, cpp_type=1, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=233,
+  serialized_end=278,
+)
+
 DESCRIPTOR.message_types_by_name['Numbers'] = _NUMBERS
 DESCRIPTOR.message_types_by_name['Predictions'] = _PREDICTIONS
+DESCRIPTOR.message_types_by_name['RecommandQuizRequest'] = _RECOMMANDQUIZREQUEST
+DESCRIPTOR.message_types_by_name['RecommandQuizResponse'] = _RECOMMANDQUIZRESPONSE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Numbers = _reflection.GeneratedProtocolMessageType('Numbers', (_message.Message,), {
@@ -126,6 +211,20 @@ Predictions = _reflection.GeneratedProtocolMessageType('Predictions', (_message.
   })
 _sym_db.RegisterMessage(Predictions)
 
+RecommandQuizRequest = _reflection.GeneratedProtocolMessageType('RecommandQuizRequest', (_message.Message,), {
+  'DESCRIPTOR' : _RECOMMANDQUIZREQUEST,
+  '__module__' : 'predict.predict_pb2'
+  # @@protoc_insertion_point(class_scope:predict.RecommandQuizRequest)
+  })
+_sym_db.RegisterMessage(RecommandQuizRequest)
+
+RecommandQuizResponse = _reflection.GeneratedProtocolMessageType('RecommandQuizResponse', (_message.Message,), {
+  'DESCRIPTOR' : _RECOMMANDQUIZRESPONSE,
+  '__module__' : 'predict.predict_pb2'
+  # @@protoc_insertion_point(class_scope:predict.RecommandQuizResponse)
+  })
+_sym_db.RegisterMessage(RecommandQuizResponse)
+
 
 DESCRIPTOR._options = None
 
@@ -135,8 +234,8 @@ _PREDICTOR = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=135,
-  serialized_end=202,
+  serialized_start=281,
+  serialized_end=430,
   methods=[
   _descriptor.MethodDescriptor(
     name='GetPredict',
@@ -145,6 +244,15 @@ _PREDICTOR = _descriptor.ServiceDescriptor(
     containing_service=None,
     input_type=_NUMBERS,
     output_type=_PREDICTIONS,
+    serialized_options=None,
+  ),
+  _descriptor.MethodDescriptor(
+    name='RecommandQuiz',
+    full_name='predict.Predictor.RecommandQuiz',
+    index=1,
+    containing_service=None,
+    input_type=_RECOMMANDQUIZREQUEST,
+    output_type=_RECOMMANDQUIZRESPONSE,
     serialized_options=None,
   ),
 ])
